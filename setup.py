8a967e7162bfef79d3711dd243c38e152000c742
idin <py@idin.ca>
2020-04-23 10:18:44 -0700

    version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
 	name='abstract',
-	version='2020.3.9',
+	version='2020.4.6',
 	license='MIT',
 	author='Idin',
 	author_email='py@idin.ca',
